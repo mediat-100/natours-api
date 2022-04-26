9db5df7749bde1c4b326269fc9e309e0c806758c
mediat-100 <yusuftomiwa740@gmail.com>
2022-04-26 17:49:01 +0100

    Added cookie to the response object

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,6 +11,16 @@ const sendToken = (user, status, statusCode, res) => {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
   user.password = undefined;
 
   return res.status(statusCode).json({
